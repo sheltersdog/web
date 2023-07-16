b47c9d7e8b52d68e7078908435d2b0ecb2d3305e
mysend12 <mysend12@naver.com>
2023-07-16 22:30:52 +0900

    fix: API 경로 수정

--- src/api/addressApi.ts
+++ src/api/addressApi.ts
@@ -2,6 +2,5 @@ import { http } from "."
 
 
 export const getAddresses = async ({type, parentCode, keyword}: any) => {
-  // return await http.get(`/api/shelter/address/list?type=${type??''}&parentCode=${parentCode??''}&keyword=${keyword??''}`)
-  return await http.get(`/address/list?type=${type??''}&parentCode=${parentCode??''}&keyword=${keyword??''}`)
+  return await http.get(`/api/shelter/address/list?type=${type??''}&parentCode=${parentCode??''}&keyword=${keyword??''}`)
 }
